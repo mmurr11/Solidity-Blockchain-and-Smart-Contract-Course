4230c2cc9887c0eba49d9b4ac4d7ea22d3ead10c
mmurr11 <86740193+mmurr11@users.noreply.github.com>
2021-09-13 10:54:41 -0700

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,21 @@
+from solcx import compile_standard
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+# Solidity source code
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+print(compiled_sol)
